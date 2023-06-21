5192e1464bce8949803d31b2fbc04ff84e39b916
lawrence-254 <lodhiambo26@gmail.com>
2023-06-21 02:10:43 -0700

    0

---
+++ err1.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+int use_err(void);
+int malloc_err(void);
+int fopen_err(char *filename);
+int instr_err(char *opcode, unsigned int err_line);
+int push_arg_err(unsigned int err_line);
+
+/**
+ * use_err - prints errors arising from usage
+ * Return: always exits the prog
+ */
+int use_err(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	return(EXIT_FAILURE);
+}
+/**
+ * malloc_err - prints memory allocation error messages
+ * Return: always exits the prog
+ */
+int malloc_err(void)
+{
+	fprintf(stderr, "Error: mem allocation failed\n");
+	return(EXIT_FAILURE);
+}
+/**
+ * fopen_err - prints errors that arise from opening a file
+ * @filename: the file in question
+ * Return: always exits the prog
+ */
+int fopen_err(char *filename)
+{
+	fprintf(stderr, "Error: file cannot be opened\n");
+	return(EXIT_FAILURE);
+}
+/**
+ * instr_err - prints error from unregistered instruction
+ * @opcode: loction of the code giving ou errors
+ * @err_line: the line of the error
+ * Return: always 1
+ */
+int instr_err(char *opcode, unsigned int err_line)
+{
+	fprintf(stderr, "L%u: unidentified command %s\n", err_line, opcode);
+	return(EXIT_FAILURE);
+}
+/**
+ * push_arg_err - prints invalid push error messages
+ * @err_line: the line of the error
+ * Return: always exit 1
+ */
+int push_arg_err(unsigned int err_line)
+{
+	fprintf(stderr, "L%u: usage: push command invalid\n", err_line);
+	return(EXIT_FAILURE);
+}
---
+++ err2.c
@@ -0,0 +1,59 @@
+#include "monty.h"
+int pop_err(unsigned int err_line);
+int show_empty_err(unsigned int err_line);
+int math_stack_err(unsigned int err_line, char *cm);
+int div_err(unsigned int err_line);
+int char_err(unsigned int err_line, char *m);
+/**
+ * pop_err - prints error when you try to pop empty stacks
+ * @err_line: the line of error
+ * Return: always exit 1
+ */
+int pop_err(unsigned int err_line)
+{
+	fprintf(stderr, "L%u: Stack is empty, unable to pop\n", err_line);
+	return (EXIT_FAILURE);
+}
+/**
+ * show_empty_err - prints error when empty stacks
+ * @err_line: the line of error
+ * Return: always exit 1
+ */
+int show_empty_err(unsigned int err_line)
+{
+	fprintf(stderr, "L%d: Stack is empty, unable to print\n", err_line);
+	return (EXIT_FAILURE);
+}
+/**
+ * math_stack_err - shows math errors for stack/queus smaller than two nodes
+ * @err_line: the line of error
+ * @cm: command that resulted to error
+ * Return: always exit 1
+ */
+int math_stack_err(unsigned int err_line, char *cm)
+{
+	fprintf(stderr, "L%u: Unable to %s, short stacks/queues\n",
+			err_line, cm);
+	return (EXIT_FAILURE);
+}
+/**
+ * div_err - prints error when you try to divide by zero
+ * @err_line: the line of error
+ * Return: always exit 1
+ */
+int div_err(unsigned int err_line)
+{
+	fprintf(stderr, "L%u: unable to divide by zero\n", err_line);
+	return (EXIT_FAILURE);
+}
+/**
+ * char_err - show math error for stack/queu where there is invalid character
+ * @err_line: the line of error
+ * @m: error message to be printed
+ * Return: always exit 1
+ */
+int char_err(unsigned int err_line, char *m)
+{
+	fprintf(stderr, "L%u: Unable to print char%s\n", err_line, cm);
+	return (EXIT_FAILURE);
+}
